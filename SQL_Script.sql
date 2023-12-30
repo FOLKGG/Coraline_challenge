e6738e2113ce0b19e452593310264816ed9925b3
FOLK <thanyathon.s@kkumail.com>
2023-12-30 21:14:34 +0700

    feature/SQL_Script.sql ver.1.0

---
+++ SQL_Script.sql
@@ -0,0 +1,7 @@
+Select category AS Category,
+       CAST(SUM(totalprice) FILTER (WHERE region = 'East') AS integer) AS East,
+       CAST(SUM(totalprice) FILTER (WHERE region = 'West') AS integer) AS West,
+       CAST(SUM(totalprice) As integer) as Grand_Total
+FROM food_sales
+GROUP BY category
+Order By category;
