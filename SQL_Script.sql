0e72435c836c7237e656395775a5d7c702ae3b82
FOLK <thanyathon.s@kkumail.com>
2024-01-01 01:20:09 +0700

    feature/ add config file to python script

--- SQL_Script.sql
+++ SQL_Script.sql
@@ -2,7 +2,7 @@ CREATE TABLE cat_reg AS
 Select category AS Category,
        CAST(SUM(totalprice) FILTER (WHERE region = 'East') AS integer) AS East,
        CAST(SUM(totalprice) FILTER (WHERE region = 'West') AS integer) AS West,
-       CAST(SUM(totalprice) As integer) as Grand_Total
+       CAST(SUM(totalprice) As integer) AS Grand_Total
 FROM food_sales
 GROUP BY category
 Order By category;
